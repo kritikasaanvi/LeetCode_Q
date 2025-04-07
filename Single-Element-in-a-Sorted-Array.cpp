8e561a6e6e497a4487ec7771e315aa84284cfdf9
kritikasaanvi <136021810+kritikasaanvi@users.noreply.github.com>
2025-04-07 11:51:12 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 67.89%] - LeetPush

---
+++ Single-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() -1;
+        
+        while(high>low){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == nums[mid^1]){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
